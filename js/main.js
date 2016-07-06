9d636e7916eb48239c8f6c18026c26ab290e248c
tjsxs <tejasxs@gmail.com>
2016-07-06 13:16:23 +0530

    Completed edit item functionality

--- js/main.js
+++ js/main.js
@@ -14,10 +14,12 @@ var generateItem = function(){
     var checkBox = document.createElement("input");
         checkBox.type="checkbox";
     
-    var editButton = document.createElement("button");
+    var editButton = document.createElement("input");
+        editButton.type="button";
         editButton.className="btn edit-btn";
     
-    var deleteButton = document.createElement("button");
+    var deleteButton = document.createElement("input");
+        deleteButton.type="button";
         deleteButton.className="btn delete-btn";
     
     var editInput = document.createElement("input");
@@ -33,6 +35,7 @@ var generateItem = function(){
     document.getElementById("input-value").value="";  
     
     deleteButton.addEventListener("click", deleteItem);
+    editButton.addEventListener("click", editItem);
 }
 
 
@@ -42,4 +45,25 @@ document.getElementById("submit-btn").addEventListener("click", generateItem);
 //Delete item
 function deleteItem(){
     this.parentNode.parentNode.removeChild(this.parentNode);
+}
+
+// Edit list Item
+function editItem(){
+    //var temp = this.parentNode.children[1];
+    var li = this.parentNode,
+        liChild = li.childNodes[1],
+        ip = document.createElement("input"),
+        label = document.createElement("label");
+    
+    if(liChild.tagName == "LABEL"){
+        ip.value = liChild.textContent;
+        ip.type="text";
+        ip.className="on-edit";
+        li.replaceChild(ip, li.childNodes[1]);
+        li.className = "temp-li";
+    }else{
+        label.textContent = liChild.value;
+        li.replaceChild(label, li.childNodes[1]);
+        li.className = "";
+    }
 }
